53eb0cf782af0bd5ac8c6a7138ee464fe3961714
Eshwarappa <eshwarappa2001@gmail.com>
2024-06-15 11:43:13 +0530

    bloodgrup changes

--- abstract1/src/com/xworkz/interference/constants/BloodGroup.java
+++ abstract1/src/com/xworkz/interference/constants/BloodGroup.java
@@ -2,5 +2,5 @@ package com.xworkz.interference.constants;
 
 public enum BloodGroup {
 
-    Apos , Aneg , Bpos , Bneg , ABpos , ABneg , Opos , Oneg ,A , B , O ;
+    Apos , Aneg , Bpos , Bneg , ABpos , ABneg , Opos , Oneg ,A , B , O ,G,R,S,Z;
 }
